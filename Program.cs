2c7162eed2d53bb8ff51636549aaf5935c3d69e4
Pavel Zanchuk <buzzik@yahoo.com>
2019-02-11 21:08:07 -0800

    Refactor code and add funtionality for Integer input

--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@ using System;
 
   class Program
   {
-
-    public static bool IsPalindrome(string phrase)
+// ==========================String Checker Method==================================
+    public static bool IsStringPalindrome(string phrase)
     {
       int i = 0;
       int j = phrase.Length - 1;
@@ -18,22 +18,55 @@ using System;
         }
 
       return true;
-
+    }
+// ==========================Integer Checker Method==================================
+    public static bool IsIntegerPalindrome(int number)
+    {
+      int holder = number;
+      int num = 0;
+      while(number != 0)
+      {
+        num = (num * 10) + (number % 10) ;
+        number = number / 10;
+      }
+      if(num == holder)
+      {
+        return true;
+      }else
+      {
+        return false;
+      }
+    }
+// ==========================Check Again Method==================================
+    public static void CheckAgain()
+    {
+      Console.WriteLine("Would you like to check again? (Y or N): ");
+      string yesOrNo = Console.ReadLine();
+      if(yesOrNo == "Y" || yesOrNo == "y")
+      {
+        Main();
+      }
     }
 
     public static void Main()
     {
 
-      Console.WriteLine("====================================");
-      Console.WriteLine("Please enter a word/phrase: ");
-      string input = Console.ReadLine();
-      string inputNoSpaces = input.Replace(" ", string.Empty).ToLower();
-      Console.WriteLine(IsPalindrome(inputNoSpaces));
-      Console.WriteLine("Would you like to check another string? (Y or N): ");
-      string yesORno = Console.ReadLine();
-      if(yesORno == "Y" || yesORno =="y")
+      Console.WriteLine("=================Palindrome Checker===================");
+      Console.WriteLine("Would you like to check a string or an Integer? (S for String/ I for Integer)");
+      string stringOrInteger = Console.ReadLine();
+      if( stringOrInteger == "S")
       {
-        Main();
+        Console.WriteLine("Please enter a word/phrase: ");
+        string input = Console.ReadLine();
+        string inputNoSpaces = input.Replace(" ", string.Empty).ToLower();
+        Console.WriteLine(IsStringPalindrome(inputNoSpaces));
+        CheckAgain();
+      }else
+      {
+        Console.WriteLine("Please enter a number: ");
+        int inputForInteger = int.Parse(Console.ReadLine());
+        Console.WriteLine(IsIntegerPalindrome(inputForInteger));
+        CheckAgain();
       }
     }
 }
